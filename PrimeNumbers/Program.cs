c4235cbb8c8e806c8b6e0fc3a9159694a1c05932
LSLIM <LSLIM@LS>
2018-05-06 12:14:02 +0200

    I put for loop and if statements in the application

--- PrimeNumbers/Program.cs
+++ PrimeNumbers/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace PrimeNumbers
 {
-    class Program
+    /* Create a console application that prints all the prime numbers,
+        starting from 1 to 100. Use a for loop for this purpose */
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            bool isPrime = true;
+            for (int i = 2; i <= 100; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    if (j % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.WriteLine(i);
+                }
+                isPrime = true;
+            }
+            Console.ReadLine();
         }
     }
 }
